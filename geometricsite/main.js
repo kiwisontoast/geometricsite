9b352b528eb1e095e2b984c2e836e5d9e12cb34c
kiwisontoast <devbshroff@gmail.com>
2022-11-22 23:39:12 -0600

    remove repeating combinations

--- geometricsite/main.js
+++ geometricsite/main.js
@@ -1,19 +1,27 @@
 const wrapper=document.getElementById("wrapper");
 const rand=(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
+
+const uniqueRand=(min,max,prev)=>{
+  let next=prev;
+
+  while(prev===next)next=rand(min,max);
+
+  return next;
+}
 const combinations=[
   {configuration:1,roundness:1},
   {configuration:1,roundness:2},
   {configuration:1,roundness:4},
   {configuration:2,roundness:2},
   {configuration:2,roundness:3},
-  // {configuration:3,roundness:3},
 ];
-
+let prev=0;
 setInterval(()=>{
-  const index = rand(0,combinations.length-1);
+  const index = uniqueRand(0,combinations.length-1,prev);
   let combination=combinations[index];
 
   wrapper.dataset.configuration=combination.configuration;
   wrapper.dataset.roundness=combination.roundness;
   console.log(combination);
-}, 3000);
+  prev=index;
+}, 2500);
