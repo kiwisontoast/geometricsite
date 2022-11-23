524a089466fd79e2c533d8a0945071a3ec559c59
kiwisontoast <devbshroff@gmail.com>
2022-11-22 22:53:28 -0600

    js configs

--- geometricsite/main.js
+++ geometricsite/main.js
@@ -1,3 +1,19 @@
 import './style.css'
 const wrapper=document.getElementById("wrapper");
-const rand=(min,max)
+const rand=(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
+const combinations=[
+  {configuration:1,roundness:1},
+  {configuration:1,roundness:2},
+  {configuration:1,roundness:4},
+  {configuration:2,roundness:2},
+  {configuration:2,roundness:3},
+  {configuration:3,roundness:3},
+];
+
+setInterval(()=>{
+  const index = rand(0,combination.length-1),
+  combination=combination[index];
+
+  wrapper.dataset.configuration=combination.configuration;
+  wrapper.dataset.roundness=combination.roundness;
+}, 3000);
