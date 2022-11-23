52f77363a919942bd6d8c79c6d016233d76bba6c
kiwisontoast <devbshroff@gmail.com>
2022-11-22 23:31:28 -0600

    removing bugs

--- geometricsite/main.js
+++ geometricsite/main.js
@@ -1,4 +1,3 @@
-import './style.css'
 const wrapper=document.getElementById("wrapper");
 const rand=(min,max)=>Math.floor(Math.random()*(max-min+1)+min);
 const combinations=[
@@ -7,13 +6,14 @@ const combinations=[
   {configuration:1,roundness:4},
   {configuration:2,roundness:2},
   {configuration:2,roundness:3},
-  {configuration:3,roundness:3},
+  // {configuration:3,roundness:3},
 ];
 
 setInterval(()=>{
-  const index = rand(0,combination.length-1),
-  combination=combination[index];
+  const index = rand(0,combinations.length-1);
+  let combination=combinations[index];
 
   wrapper.dataset.configuration=combination.configuration;
   wrapper.dataset.roundness=combination.roundness;
+  console.log(combination);
 }, 3000);
